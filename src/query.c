e8c7fbe47f839a41ad0514f83bc9823c3daab990
usrecnik <urh@srecnik.info>
2018-01-22 13:22:44 +0100

    fixed #6 (again)

--- src/query.c
+++ src/query.c
@@ -872,9 +872,9 @@ static int qry_objects_filesize(t_fsentry *schema, t_fsentry *type) {
 
 int qry_objects(t_fsentry *schema, t_fsentry *type) {
     int retval = EXIT_SUCCESS;
-    char *query = "select \
-object_name, to_char(last_ddl_time, 'yyyy-mm-dd hh24:mi:ss') as t_modified \
-from all_objects where owner=:bind_owner and object_type=:bind_type";    
+    char query[500] = "select \
+o.object_name, to_char(o.last_ddl_time, 'yyyy-mm-dd hh24:mi:ss') as t_modified \
+from all_objects o where o.owner=:bind_owner and o.object_type=:bind_type";
 
     OCIStmt   *o_stm = NULL;
     OCIDefine *o_def[2] = {NULL, NULL};
@@ -884,8 +884,6 @@ from all_objects where owner=:bind_owner and object_type=:bind_type";
     char *schema_name = strdup(schema->fname);
     char *type_name = strdup(type->fname);
     char *suffix = NULL;
-    int is_schema_sys = 0;
-    int is_type_type = 0;
     
     if (type_name == NULL || schema_name == NULL) {
         logmsg(LOG_ERROR, "qry_objects() - Unable to strdup type_name and/or schema_name");
@@ -900,12 +898,12 @@ from all_objects where owner=:bind_owner and object_type=:bind_type";
         str_upper(type_name);
     }
     
-    if (strcmp(schema_name, "SYS") == 0) 
-        is_schema_sys = 1;
-
-    if (strcmp(type_name, "TYPE") == 0)
-        is_type_type = 1;
-     
+    if ((strcmp(schema_name, "SYS") == 0) && (strcmp(type_name, "TYPE") == 0)) {
+        strcat(query, " and exists (\
+select 1 from all_source s \
+where s.owner='SYS' and s.\"TYPE\"='TYPE' AND s.\"NAME\"=o.object_name)");
+    }
+    
     if (str_suffix(&suffix, type_name) != EXIT_SUCCESS) {
         logmsg(LOG_ERROR, "qry_objects() - Unable to obtain suffix for object type [%s]", type_name);
         if (type_name != NULL)
@@ -968,12 +966,6 @@ from all_objects where owner=:bind_owner and object_type=:bind_type";
     }
 
     while (ora_stmt_fetch(o_stm) == OCI_SUCCESS) {
-
-        if ((is_schema_sys == 1) && (is_type_type == 1)) {
-            // because SYS.BFILE of type TYPE has no source available.
-            if (strcmp((char*)o_sel[0], "BFILE") == 0)
-                continue;
-        }
 
         memset(temptime, 0, sizeof(struct tm));
         char* xx = strptime(((char*)o_sel[1]), "%Y-%m-%d %H:%M:%S", temptime);
