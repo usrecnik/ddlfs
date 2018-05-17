8feb5ea02800b11d1a10643bd564fc5d8e4669f3
usrecnik <urh@srecnik.info>
2018-05-17 10:36:18 +0200

    2.0-RC3

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@
 #include "vfs.h"
 #include "tempfs.h"
 
-#define DDLFS_VERSION "2.0-RC2"
+#define DDLFS_VERSION "2.0-RC3"
 
 int main(int argc, char *argv[]) {
     memset(&g_conf, 0, sizeof(g_conf));
