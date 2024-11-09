c7b8796af6705edb30c1ac33b28763a89a63307c
usrecnik <urh@srecnik.info>
2024-11-09 12:28:34 +0100

    better handling of PATH in windows

--- win/ddlfs.bat
+++ win/ddlfs.bat
@@ -1,9 +1,3 @@
 @echo off
 
-rem check if "C:\Program Files\ddlfs\instant_client\" is already in PATH
-echo %PATH% | find /I "C:\Program Files\ddlfs\instant_client\" > nul
-if %errorlevel% neq 0 (
-    set PATH=C:\Program Files\ddlfs\instant_client\;%PATH%
-)
-
-"C:\Program Files\ddlfs\ddlfs_.exe" %*
+set PATH=C:\Program Files\ddlfs\instant_client\;%PATH% && "C:\Program Files\ddlfs\ddlfs_.exe" %*
